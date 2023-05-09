ebec41c3b75a5ccb9746ef9899e2cc7905a6a122
britmelton <brittany134340@gmail.com>
2023-05-09 13:04:14 -0400

    add ketchup test

---
+++ bit-masking-demo/Domain/Sandwich.cs
@@ -0,0 +1,13 @@
+﻿namespace bit_masking_demo.Domain
+{
+    public class Sandwich
+    {
+        public bool HasKetchup { get; set; }
+
+        public void AddKetchup()
+        {
+            HasKetchup = true;
+        }
+
+    }
+}
---
+++ bit-masking-demo/SandwichSpec.cs
@@ -0,0 +1,21 @@
+﻿using bit_masking_demo.Domain;
+using FluentAssertions;
+
+namespace bit_masking_demo
+{
+    public class SandwichSpec
+    {
+        public class WhenAddingKetchup
+        {
+            [Fact]
+            public void ThenSandwichHasKetchup()
+            {
+                var sandwich = new Sandwich();
+
+                sandwich.AddKetchup();
+
+                sandwich.HasKetchup.Should().BeTrue();
+            }
+        }
+    }
+}
